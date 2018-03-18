54f40779ed7d1dfd002d86bcaac114b0d98a8866
Diego H. Gurpegui <diego.gurpegui@improvein.com>
2018-03-18 19:18:29 -0300

    Error handling

--- web/src/main.js
+++ web/src/main.js
@@ -10,11 +10,19 @@ $(function () {
         let content = $('#encrypt-content-input').val()
         let password = $('#encrypt-password-input').val()
 
-        let encryptedContent = encryption.encrypt(content, password)
-        $('#encrypt-result-content').text(encryptedContent)
+        try {
+            let encryptedContent = encryption.encrypt(content, password)
+            $('#encrypt-result-content').text(encryptedContent)
 
-        let now = new Date()
-        $('#encrypt-date').text(now.toString())
+            let now = new Date()
+            $('#encrypt-date').text(now.toString())
+
+            // clear error msg
+            $('#encrypt-error').text("")
+        } catch (err) {
+            console.error('There was an error encrypting', err);
+            $('#encrypt-error').text("There was an error encrypting. " + err.message)
+        }
 
         return false;
     })
@@ -26,11 +34,19 @@ $(function () {
         let content = $('#decrypt-content-input').val()
         let password = $('#decrypt-password-input').val()
 
-        let decryptedContent = encryption.decrypt(content, password)
-        $('#decrypt-result-content').text(decryptedContent)
+        try {
+            let decryptedContent = encryption.decrypt(content, password)
+            $('#decrypt-result-content').text(decryptedContent)
 
-        let now = new Date()
-        $('#decrypt-date').text(now.toString())
+            let now = new Date()
+            $('#decrypt-date').text(now.toString())
+
+            // clear error msg
+            $('#decrypt-error').text("")
+        } catch (err) {
+            console.error('There was an error decrypting', err);
+            $('#decrypt-error').text("There was an error decrypting. " + err.message)
+        }
 
         return false;
     })
