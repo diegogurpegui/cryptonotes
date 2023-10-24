a98dff42e560e7c4f5c5b69547acce70053c7d97
Diego H. Gurpegui <diego.gurpegui@improvein.com>
2023-10-24 09:55:55 -0300

    upgrade node and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    mode: 'production',
-    entry: ["./web/src/main.js", "./modules/encryption.js"],
-    output: {
-        path: path.resolve(__dirname, "web"),
-        filename: 'js/bundle.js'
-    }
+  mode: "production",
+  target: "web",
+  entry: ["./web/src/main.js", "./modules/encryption.js"],
+  output: {
+    path: path.resolve(__dirname, "web"),
+    filename: "js/bundle.js",
+  },
+  resolve: {
+    fallback: {
+      buffer: require.resolve("buffer/"),
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
 };
